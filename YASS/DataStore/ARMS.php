0d41c86771990c5ff2d85aa25081e30841b0fbea
Tim Otten <to-git@think.hm>
2011-11-12 23:59:12 +0000

    refs #1242 - yass:  * Move SQL I/O helper (nativeAdd, nativeUpdate, etc) from YASS_DataStore_ARMS to arms_util.thinapi.inc  * Modify YASS_Replica_ARMS_Test to create fixtures using thinapi, thereby fixing the handling of multi-select/checkbox fields

--- YASS/DataStore/ARMS.php
+++ YASS/DataStore/ARMS.php
@@ -81,18 +81,29 @@ class YASS_DataStore_ARMS extends YASS_DataStore {
 			// restore it, the nativeUpdate may not be
 			// sufficient
 			
-			$dataByGroup = arms_util_array_partition_func($entity->data, array($this, 'getFieldGroup'));
-			
 			list ($type, $lid) = $this->replica->mapper->toLocal($entity->entityGuid);
 			if (! ($type && $lid)) {
-				$lid = $this->nativeAdd($entity->entityType, $dataByGroup['core']);
+				db_query('SET @yass_disableTrigger = 1');
+				$result = arms_util_thinapi(array(
+					'entity' => $entity->entityType,
+					'action' => 'insert',
+					'data' => $entity->data,
+				));
+				db_query('SET @yass_disableTrigger = NULL');
+				$lid = $result['data']['id'];
 				$this->replica->mapper->addMappings(array(
 					$entity->entityType => array($lid => $entity->entityGuid)
 				));
 			} else {
-				$this->nativeUpdate($type, $lid, $dataByGroup['core']);
+				db_query('SET @yass_disableTrigger = 1');
+				$result = arms_util_thinapi(array(
+					'entity' => $entity->entityType,
+					'action' => 'update',
+					'data' => $entity->data + array('id' => $lid),
+				));
+				db_query('SET @yass_disableTrigger = NULL');
 			}
-			$this->nativeCustomSave($type, $lid, $dataByGroup);
+			
 		}
 	}
 	
@@ -121,75 +132,4 @@ class YASS_DataStore_ARMS extends YASS_DataStore {
 		return $result;
 	}
 
-	/**
-	 * Put content directly in the data store, bypassing the synchronization system.
-	 * This creates an un-synchronized entity.
-	 *
-	 * @return int, local id
-	 */
-	function nativeAdd($type, $data) {
-		$idColumn = 'id';
-		db_query('SET @yass_disableTrigger = 1');
-		db_query(arms_util_insert($type)->addValues($data)->toSQL());
-		$lid = db_last_insert_id($type, $idColumn);
-		db_query('SET @yass_disableTrigger = NULL');
-		return $lid;
-	}
-
-	/**
-	 * Put content directly in the data store, bypassing the synchronization system.
-	 * This creates an un-synchronized entity.
-	 *
-	 * @param $lid int, local id
-	 */
-	function nativeUpdate($type, $lid, $data) {
-		$idColumn = 'id';
-		db_query('SET @yass_disableTrigger = 1');
-		db_query(arms_util_update($type)->addWheref("${idColumn}=%d", $lid)->addValues($data)->toSQL());
-		db_query('SET @yass_disableTrigger = NULL');
-	}
-	
-	/**
-	 * Insert or update any custom-value records
-	 *
-	 * @param $type string
-	 * @param $lid int the ID of the primary entity
-	 * @param $dataByGroup array(groupName => array(fieldId => fieldValue))
-	 */
-	function nativeCustomSave($type, $lid, $dataByGroup) {
-		foreach ($dataByGroup as $groupName => $groupValues) {
-			if ($groupName == 'core') continue;
-			$group = arms_util_group($groupName);
-			$columnValues = arms_util_array_rekey_rosetta($group['fields'], '_param', 'column_name', $groupValues);
-
-			$idColumn = 'entity_id';
-			$insert = arms_util_insert($group['table_name'], 'update')
-				->addValue($idColumn, $lid, 'insert-only')
-				->addValues($columnValues, 'insert-update');
-			
-			db_query('SET @yass_disableTrigger = 1');
-			db_query($insert->toSQL());
-			db_query('SET @yass_disableTrigger = NULL');
-		}
-	}
-	
-	/**
-	 * Determine which custom-data group which stores a given field
-	 *
-	 * @param $key field name, e.g. 'first_name' or 'custom_123'
-	 * @param $value field value
-	 * @return FALSE, 'core', or group-name
-	 */
-	function getFieldGroup($key, $value) {
-		if (preg_match('/^custom_(\d+)$/', $key, $matches)) {
-			$field = arms_util_field_by_id($matches[1]);
-			if (is_array($field)) {
-				return $field['_group_name'];
-			} else {
-				return FALSE;
-			}
-		} else {
-			return 'core';
-		}
-	}
 }
