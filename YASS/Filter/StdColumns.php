9165b06d4805fc0e6465d6538950b572adaafa7a
Tim Otten <to-git@think.hm>
2011-12-10 05:08:29 +0000

    refs #1242 - yass - Implement YASS_Filter_StdColumns
    This can control the visibility of certain data columns (e.g. a site can see its own custom-data but not other sites' custom-data)

---
+++ YASS/Filter/StdColumns.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'YASS/Filter.php';
+
+/**
+ * Filter out columns which should not be visible based on (our initial standard) security policy.
+ *
+ *   data['#custom']             --> keep
+ *   data['#unknown']['mysite']  --> keep
+ *   data['#unknown']['other']   --> remove
+ *   data['#acl']                --> remove
+ *
+ * This implementation will likely be replaced by something more sophisticated as our
+ * requirements grow.
+ */
+class YASS_Filter_StdColumns extends YASS_Filter {
+
+  /**
+   *
+   * @param $spec array; keys:
+   */
+  function __construct($spec) {
+    parent::__construct($spec);
+  }
+  
+  function toLocal(&$entities, YASS_Replica $to) {
+    foreach ($entities as $entity) {
+      if (is_array($entity->data['#unknown'])) {
+        foreach (array_keys($entity->data['#unknown']) as $key) {
+          if ($key == $to->name) continue;
+          unset($entity->data['#unknown'][$key]);
+        }
+      }
+    }
+  }
+  
+  // Master is allowed to see everything coming from replica
+  // function toGlobal(&$entities, YASS_Replica $from) {
+  // }
+}
