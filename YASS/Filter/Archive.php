7c5092f0783dd25ff73ff527d72453ea3196bd69
Tim Otten <to-git@think.hm>
2012-01-03 00:11:02 +0000

    refs #1242, CAP-94, CAP-93 - Implement YASS_Filter_Archive:  * Create SQL table, yass_archive  * Record timestamps as part of entity archives

--- YASS/Filter/Archive.php
+++ YASS/Filter/Archive.php
@@ -40,6 +40,7 @@ class YASS_Filter_Archive extends YASS_Filter {
 				'u_replica_id' => $version->replicaId,
 				'u_tick' => $version->tick,
 				'data' => serialize($entity->data),
+				'timestamp' => arms_util_time(),
 			);
 			drupal_write_record('yass_archive', $archive);
 		}
