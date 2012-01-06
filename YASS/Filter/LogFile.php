16ef10533d2b97dd8cbae3d9280ac932dc9a0576
Tim Otten <to-git@think.hm>
2012-01-06 10:12:25 +0000

    refs #1242 - yass_replica_logall - Add a helper module which logs all incoming/outgoing entities to CSV

---
+++ YASS/Filter/LogFile.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once 'YASS/Context.php';
+require_once 'YASS/Filter.php';
+
+/**
+ * Record a log note about every record that passes through
+ */
+class YASS_Filter_LogFile extends YASS_Filter {
+
+	/**
+	 *
+	 * @param $spec array; keys: 
+	 *  - file: string, file path
+	 */
+	function __construct($spec) {
+		parent::__construct($spec);
+		arms_util_include_api('array');
+	}
+	
+	function log($op, $entities, YASS_Replica $replica) {
+		$needsHeader = !file_exists($this->spec['file']);
+		$fh = fopen($this->spec['file'], 'a');
+		if ($needsHeader) {
+			fputcsv($fh, array(
+				'timestamp',
+				'transferId',
+				'replica',
+				'operation',
+				'entityGuid',
+				'entityType',
+				'entityExists',
+				'entityData',
+			));
+		}
+		$host = defined('DRUSH_URI') ? DRUSH_URI : $_SERVER['HTTP_HOST'];
+		foreach ($entities as $entity) {
+			fputcsv($fh, array(
+				date('Y-m-d H:i:s', arms_util_time()),
+				YASS_Context::get('transferId'),
+				sprintf('%s@%s <#%s>', $replica->name, $host, $replica->id),
+				$op,
+				$entity->entityGuid,
+				$entity->entityType,
+				$entity->exists,
+				json_encode($entity->data),
+			));
+		}
+		fclose($fh);
+		if (isset($this->spec['mode'])) {
+			chmod($this->spec['file'], $this->spec['mode']);
+		}
+	}
+	
+	function toGlobal(&$entities, YASS_Replica $replica) {
+		$this->log('read', $entities, $replica);
+	}
+	
+	function toLocal(&$entities, YASS_Replica $replica) {
+		$this->log('write', $entities, $replica);
+	}
+}
