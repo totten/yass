7d63b2337590c248bb20180d7668798d44b329a0
Tim Otten <to-git@think.hm>
2012-02-23 07:10:05 -0500

    CAP-153 - When sport/gender is missing for a contact from a multi-sport site, fill it in.

--- YASS/Filter/GenderSportRelations.php
+++ YASS/Filter/GenderSportRelations.php
@@ -46,6 +46,8 @@ class YASS_Filter_GenderSportRelations extends YASS_Filter {
         parent::__construct($spec);
         arms_util_include_api('array');
         require_once 'YASS/Engine.php';
+        require_once 'YASS/Log.php';
+        $this->_log = YASS_Log::instance('YASS_Filter_GenderSportRelations');
     }
     
     /**
@@ -81,13 +83,18 @@ class YASS_Filter_GenderSportRelations extends YASS_Filter {
         foreach ($entities as $entity) {
             if (!$entity->exists) continue;
             if ($this->spec['entityType'] == $entity->entityType) {
-                if ($entity->data['#custom']['secGender'] && $entity->data['#custom']['secSport']) {
-                    $secValues[$entity->entityGuid] = array(
-                        'secGender' => $entity->data['#custom']['secGender'],
-                        'secSport' => $entity->data['#custom']['secSport'],
-                    );
-                    continue;
+                if (! ($entity->data['#custom']['secGender'] && $entity->data['#custom']['secSport'])) {
+                    $this->_log->infof('toGlobal(%s): fill in sport(%s) and gender(%s)', $entity->entityGuid, $this->spec['fallback']['secSport'], $this->spec['fallback']['secGender']);
+                    // ex: old record which never had its secGender/secSport set
+                    $entity->data['#custom']['secGender'] = $this->spec['fallback']['secGender'];
+                    $entity->data['#custom']['secSport'] = $this->spec['fallback']['secSport'];
                 }
+                $secValues[$entity->entityGuid] = array(
+                    'secGender' => $entity->data['#custom']['secGender'],
+                    'secSport' => $entity->data['#custom']['secSport'],
+                );
+                continue;
+                // */
             }
             if ($relationColumn = $this->spec['relations'][$entity->entityType]) {
                 if ($entity->data[$relationColumn]) {
@@ -123,7 +130,7 @@ class YASS_Filter_GenderSportRelations extends YASS_Filter {
                     // e.g. civicrm_{email,address}.contact_id is blank for domain records
                     $secValue = $this->spec['fallback'];
                 }
-                if (empty($secValue)) {
+                if (empty($secValue) || empty($secValue['secGender']) || empty($secValue['secSport'])) {
                     throw new Exception(sprintf('Failed to determine secValue for replica=[%s] entityType=[%s] entityGuid=[%s] relationCol=[%s] relationGuid=[%s]',
                         $replica->name,
                         $entity->entityType,
